11826d07e8df6fad889ae6dad49fcab06f4431f7
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2025-04-15 11:51:02 +0700

    Updated docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
+import Payment from "./core/Payment";
 import { publicAPI } from "./core/publicAPI";
 import { streamAPI } from "./core/streamAPI";
 
 export * from "./core/streamAPI";
 export * from "./core/publicAPI";
+export * from "./core/Payment";
 
 const trakteer = {
     streamAPI,
     publicAPI,
+	Payment
 };
 export default trakteer;
