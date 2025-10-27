161eb81a9ba0de97982902f0d1d8ef12d2c12787
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2025-10-27 14:23:12 +0700

    Remove unneeded export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export * from "./core/streamAPI";
 export * from "./core/publicAPI";
-export * from "./core/Payment";
-export * from "./core/User";
 
 export { default as Payment } from "./core/Payment";
 export { default as User } from "./core/User";
