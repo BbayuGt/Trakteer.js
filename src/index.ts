d72a8ba4317c19d1ea0f67355b888512f7ecffbe
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2025-10-27 14:01:07 +0700

    export defaults

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from "./core/streamAPI";
 export * from "./core/publicAPI";
 export * from "./core/Payment";
 export * from "./core/User";
+
+export { default as Payment } from "./core/Payment";
+export { default as User } from "./core/User";
